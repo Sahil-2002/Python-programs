62f98c365cf015fe65e295366ab5fc07cfce0615
Sahil-2002 <sahila.khandekar@gmail.com>
2024-06-10 15:50:25 +0530

    Implementing concepts of dictionary in python

---
+++ dict.py
@@ -0,0 +1,6 @@
+dict={}
+
+dict[input("enter the subject")]= int(input("enter the marks "))
+dict[input("enter the subject")]= int(input("enter the marks "))
+dict[input("enter the subject")]= int(input("enter the marks "))
+print(dict)

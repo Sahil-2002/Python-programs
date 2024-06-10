3b41683ee25460cddd611e24f2b1ea9cbf1ac6c2
Sahil-2002 <sahila.khandekar@gmail.com>
2024-06-10 15:55:09 +0530

    [Sahil] Add : Learning concept of while loop in python

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+
+i=1
+j=1
+while j<100:
+   
+     j=i**2
+     print(j)
+     i =i+1
+
+# i = 1
+# while i <= 100:
+#     print(i**2)  # Directly print the square of i
+#     i += 1       # Increment i by 1
+
